d8660f350f65ce1198bca4825fdf881b005e4019
darckronoz <crispipe14@gmail.com>
2026-02-14 00:21:31 -0500

    Set up project structure and package.json

---
+++ src/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// CLI Type Game - Main Entry Point
+// This is where the game starts
+
+console.log('CLI Type Game v0.0.1');
+console.log('Starting typing practice session...\n');
+
+// TODO: Initialize game components
