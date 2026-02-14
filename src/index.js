f757598f4605a211b81c7bcfd06b075f04d0021d
darckronoz <crispipe14@gmail.com>
2026-02-14 00:38:46 -0500

    Implement keystroke input capture with raw mode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 #!/usr/bin/env node
 
 // CLI Type Game - Main Entry Point
-// This is where the game starts
+const InputHandler = require('../lib/inputHandler');
 
 console.log('CLI Type Game v0.0.1');
 console.log('Starting typing practice session...\n');
+console.log('Type something (Ctrl+C to exit):\n');
 
-// TODO: Initialize game components
+const inputHandler = new InputHandler();
+
+// Listen for character input
+inputHandler.on('onChar', (char, fullInput) => {
+  process.stdout.write(char); // Echo the character to screen
+});
+
+// Listen for backspace
+inputHandler.on('onBackspace', (fullInput) => {
+  process.stdout.write('\b \b'); // Backspace, space, backspace (erase character)
+});
+
+// Listen for enter
+inputHandler.on('onEnter', (fullInput) => {
+  console.log('\n\nYou typed: ' + fullInput);
+  process.exit(0);
+});
+
+// Listen for exit
+inputHandler.on('onExit', () => {
+  console.log('\n\nGoodbye!');
+});
+
+inputHandler.startListening();
